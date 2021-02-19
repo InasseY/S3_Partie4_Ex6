823d1e5a010f15e55ced9b19c169c3fc415ecd68
InasseY <inasse.youssouf@gmail.com>
2021-02-19 16:20:36 +0100

    correction

---
+++ Partie4_Ex6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Partie4_Ex6
+{
+    class Program
+    {
+        //variable en dehors du mains figé
+        enum week
+        {
+            Lundi,
+            Mardi,
+            Mercredi,
+            Jeudi,
+            Vendredi,
+            Samedi,
+            Dimanche
+
+        }
+        static void Main(string[] args)
+        {
+            //String[] weeks = Enum.GetNames(typeof(Week));
+
+            foreach (string day in Enum.GetNames(typeof(Week)))
+                Console.WriteLine(day);
+
+            Console.WriteLine((Week)4);
+            //Console.WriteLine(Enum.GetName(typeof(Week), 4));
+            //Console.WriteLine(Enum.GetNames(typeof(Week)).GetValue(4));   
+        }
+    }
+}
